46dce48a8e6efda7dc0518ac08b44fdf3ceb1f50
Veronika Fatikhova <fidelio@list.ru>
2021-11-02 14:57:10 -0700

    spinner.js added

---
+++ spinner1.js
@@ -0,0 +1,28 @@
+//Creats a low-fidelity loading spinner
+setTimeout(() => {
+  process.stdout.write('\r|   '); // \r returns cursor back to the beginning of the line
+}, 100);
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+setTimeout(() => {
+  process.stdout.write('\r\\   ');// escape the backslash since it's a special character
+}, 700);
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
